b367da137ccd879cd281ae80c2a8a1f34617924e
Hitesh Odedara <hitesh.odedara.dev@gmail.com>
2025-07-05 06:05:53 +0000

    make this app pwa for my use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,19 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
+  applicationName: 'Life OS',
   title: 'Life OS',
   description: 'Your all-in-one productivity assistant.',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Life OS',
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  themeColor: '#64B5F6',
 };
 
 export default function RootLayout({
@@ -15,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
